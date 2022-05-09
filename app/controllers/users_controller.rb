36b43b89162ec755fa7f675ef99160f30e4cd494
kuriyama585 <kuriyama585@yahoo.co.jp>
2022-05-09 20:39:57 +0900

    詳細ページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # @name = current_user.name
-    @prototypes = current_user.prototypes    
+    @prototypes = @user.prototypes    
     # ↑上記にusersコントローラーのshowアクションにインスタンス変数@userを定義した。且つ、
     # Pathパラメータで送信されるID値で、Userモデルの特定のオブジェクトを取得するように記述
   end
